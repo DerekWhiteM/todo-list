eabf8bcba2525b2295c572a0505c4c0ae74a6e04
Derek White <derekwhite.m@gmail.com>
2026-05-06 22:39:44 -0400

    Rename from react-todo to todo-list

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "https://derekwhitem.github.io/react-todo/"
+    base: "https://derekwhitem.github.io/todo-list/"
 });
